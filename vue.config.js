a9cb42e26acf5163b2323475cbefd20e7b7a86a5
darkmasta <alperen_bagci@hotmail.com>
2021-03-26 23:54:07 +0300

    delete dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   transpileDependencies: [
@@ -14,25 +14,27 @@ module.exports = {
     /\bmarked\b/,
     /\bvue-plyr\b/,
     /\bswiper\b/,
-    /\bdom7\b/
+    /\bdom7\b/,
   ],
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // Add "node_modules" alias
-    config.resolve.alias
-      .set('node_modules', path.join(__dirname, './node_modules'))
+    config.resolve.alias.set(
+      "node_modules",
+      path.join(__dirname, "./node_modules")
+    );
 
     // Disable "prefetch" plugin since it's not properly working in some browsers
-    config.plugins
-      .delete('prefetch')
+    config.plugins.delete("prefetch");
 
     // Do not remove whitespaces
-    config.module.rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => {
-        options.compilerOptions.whitespace = 'preserve'
-        return options
-      })
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.compilerOptions.whitespace = "preserve";
+        return options;
+      });
   },
-  lintOnSave: false
-}
+  lintOnSave: false,
+};
